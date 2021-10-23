4997b62f740d8a88e9a04351a4b12671882cc3ce
Louis Cavecchi <cavecchilouis@gmail.com>
2021-10-23 19:09:07 +0200

    Fix responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import Player from './components/Player'
+import React from "react";
+import Player from "./components/Player";
 
 const App = () => {
   return (
@@ -12,42 +12,64 @@ const App = () => {
         <section id="intro">
           <div className="intro_text">
             <h1>Maestria</h1>
-            <p>Nom féminin : Aisance, perfection dans l'exécution d'une œuvre d'art, dans la réalisation de quelque chose ; brio, virtuosité.</p>
+            <p>
+              Nom féminin : Aisance, perfection dans l'exécution d'une œuvre
+              d'art, dans la réalisation de quelque chose ; brio, virtuosité.
+            </p>
           </div>
           <img src="./images/p1.jpg" alt="" />
           <div>
             <h3 className="intro_footer">Découvrir l’histoire</h3>
-            <img className="intro_union" src="./images/union.png" alt=""/>
+            <img className="intro_union" src="./images/union.png" alt="" />
           </div>
         </section>
         <section id="story">
           <div>
-            <h2>Les Djé, c’est l’histoire de deux frères, deux frères déterminés dans leur passion...</h2>
+            <h2>
+              Les Djé, c’est l’histoire de deux frères, deux frères déterminés
+              dans leur passion...
+            </h2>
           </div>
           <img src="./images/p2.jpg" alt="" />
         </section>
         <section id="work">
           <div className="work_images">
-            <img src="./images/p3.jpg" alt=""/>
-            <img src="./images/p4.jpg" alt=""/>
+            <img src="./images/p3.jpg" alt="" />
+            <img src="./images/p4.jpg" alt="" />
           </div>
           <div>
-            <h2>...leur travail leur fit développer une maîtrise de leur art, la Maestria.</h2>
+            <h2>
+              ...leur travail leur fit développer une maîtrise de leur art, la
+              Maestria.
+            </h2>
           </div>
         </section>
         <section id="years">
           <div>
-            <h2>...leur travail leur fit développer une maîtrise de leur art, la Maestria.</h2>
+            <h2>
+              ...leur travail leur fit développer une maîtrise de leur art, la
+              Maestria.
+            </h2>
           </div>
           <div className="years_images">
-            <img src="./images/p5.jpg" alt=""/>
-            <img src="./images/p6.jpg" alt=""/>
+            <img src="./images/p5.jpg" alt="" />
+            <img src="./images/p6.jpg" alt="" />
           </div>
         </section>
-        <iframe title="Video clip" width="100%" height="500px" src="https://www.youtube.com/embed/Vqf4dPoohJA" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+        <iframe
+          title="Video clip"
+          width="100%"
+          height="500px"
+          src="https://www.youtube.com/embed/Vqf4dPoohJA"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
         <Player />
         <section id="shop">
-          <a className="shop_btn" href="https://www.google.fr">Visiter la boutique</a>
+          <a className="shop_btn" href="https://www.google.fr">
+            Visiter la boutique
+          </a>
         </section>
         <footer>
           <p>Les Djé - 2021</p>
@@ -55,6 +77,6 @@ const App = () => {
       </main>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,33 +1,57 @@
-import React from 'react'
-import { useState } from 'react'
+import React from "react";
+import { useState } from "react";
 
 const Player = () => {
-    const [link, setLink] = useState<string>('https://open.spotify.com/embed/album/29FRXjKDesQsuSMnailZI1?utm_source=generator')    
+  const [link, setLink] = useState<string>(
+    "https://open.spotify.com/embed/album/29FRXjKDesQsuSMnailZI1?utm_source=generator"
+  );
 
-    return (
-        <section id="player">
-            <h2>Nouvel album “Maestria”</h2>
-            <p>Disponible sur toutes les plateformes</p>
-            <div className="player_content">
-                <div>
-                    <img className="player_cover" src="./images/cover.jpg" alt=""/>
-                    <p>Ecouter sur :</p>
-                    <div className="player_btn">
-                        <button onClick={() => setLink('https://open.spotify.com/embed/album/29FRXjKDesQsuSMnailZI1?utm_source=generator')}>
-                            <img src="./images/spotify.png" alt=""/>
-                        </button>
-                        <button onClick={() => setLink('https://embed.music.apple.com/fr/album/d%C3%A9brouillard-ep/1530555106')}>
-                            <img src="./images/applemusic.png" alt=""/>
-                        </button>
-                        <button onClick={() => setLink('https://widget.deezer.com/widget/dark/album/171925172')}>
-                            <img src="./images/deezer.png" alt=""/>
-                        </button>
-                    </div>
-                </div>
-                <iframe title="Music Player" src={link} height="500px" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
-            </div>
-        </section>
-    );
-}
+  return (
+    <section id="player">
+      <h2>Nouvel album “Maestria”</h2>
+      <p>Disponible sur toutes les plateformes</p>
+      <div className="player_content">
+        <div>
+          <img className="player_cover" src="./images/cover.jpg" alt="" />
+          <p>Ecouter sur :</p>
+          <div className="player_btn">
+            <button
+              onClick={() =>
+                setLink(
+                  "https://open.spotify.com/embed/album/29FRXjKDesQsuSMnailZI1?utm_source=generator"
+                )
+              }
+            >
+              <img src="./images/spotify.png" alt="" />
+            </button>
+            <button
+              onClick={() =>
+                setLink(
+                  "https://embed.music.apple.com/fr/album/d%C3%A9brouillard-ep/1530555106"
+                )
+              }
+            >
+              <img src="./images/applemusic.png" alt="" />
+            </button>
+            <button
+              onClick={() =>
+                setLink("https://widget.deezer.com/widget/dark/album/171925172")
+              }
+            >
+              <img src="./images/deezer.png" alt="" />
+            </button>
+          </div>
+        </div>
+        <iframe
+          title="Music Player"
+          src={link}
+          height="500px"
+          frameBorder="0"
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        ></iframe>
+      </div>
+    </section>
+  );
+};
 
 export default Player;
